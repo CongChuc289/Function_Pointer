f034ad3c5ce9359e6d6014109d84ec9d84b4b107
congchuc289 <75417668+CongChuc289@users.noreply.github.com>
2025-02-25 09:22:27 +0900

    Update Arr_Func_Pointer.c

--- Arr_Func_Pointer.c
+++ Arr_Func_Pointer.c
@@ -1,4 +1,3 @@
-//3. Mảng con trỏ hàm
 #include <stdio.h>
 
 // Các hàm để sử dụng
@@ -12,10 +11,22 @@ int multiply(int a, int b) {
     return a * b;
 }
 
-int main() {
-    // Khai báo mảng con trỏ hàm
-    int (*operations[3])(int, int) = {add, subtract, multiply};
+void Func1(void){
+    printf("Func1\n");
+}
 
+void Func2(void){
+    printf("Func2\n");
+}
+
+// Khai báo mảng con trỏ hàm
+int (*operations[3])(int, int) = {add, subtract, multiply}; // Có tham số truyền vào
+void (*Function[2])(void) = {Func1, Func2};                 // không có tham sô truyền vào
+
+int main() {
+    Function[0]();
+    Function[1]();
+    
     // Gọi các hàm thông qua mảng con trỏ hàm
     printf("Tổng: %d\n", operations[0](10, 5));        // Output: 15
     printf("Hiệu: %d\n", operations[1](10, 5));       // Output: 5
